449412fae5ade83997f08d8bf86e25369ad2fd9c
kot9ra18 <alexandr-prokhorov@mail.ru>
2022-02-17 13:24:06 +0300

    StorageData

---
+++ DZ 14 StorageData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ 14 StorageData
+//
+//  Created by Александр Прохоров on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
